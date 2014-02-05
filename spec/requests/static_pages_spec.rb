c98e47f9b792fa17bbaeca13b62f4527f24dcbae
de.liuyd <de.liuyd@gmail.com>
2014-02-05 22:27:40 +0800

    gard test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -15,7 +17,7 @@ describe "Static Pages" do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Home")
+      page.should have_title("#{base_title} | Home")
     end
 
 
@@ -34,7 +36,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+      page.should have_title("#{base_title} | Help")
     end
   end
 
@@ -51,7 +53,7 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_title("#{base_title} | About Us")
     end
 
 
