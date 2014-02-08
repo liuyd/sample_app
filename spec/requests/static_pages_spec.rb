12ddcdf5bb41eea57414b5651b7c32616a0e7372
de.liuyd <de.liuyd@gmail.com>
2014-02-08 14:30:42 +0800

    route

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,22 +6,22 @@ describe "Static Pages" do
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_title('| Home')
     end
 
@@ -30,38 +30,47 @@ describe "Static Pages" do
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
+  end
 
-
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_title("#{base_title} | Contact")
+    end
   end
 
 end
