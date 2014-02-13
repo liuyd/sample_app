c93fb937b4adca2608e004501f8900eaa3e78153
de.liuyd <de.liuyd@gmail.com>
2014-02-13 09:44:38 +0800

    password vlidate

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,22 @@ class User < ActiveRecord::Base
   #r4:#<Class:0x52d0b38>: `attr_accessible` is extracted out of Rails into a gem. Please use new recommended protection model for params(strong_parameters) or add `protected_attributes` to your Gemfile to use old one.
   #attr_accessible :name, :email
 
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
             format: { with: VALID_EMAIL_REGEX } ,
             uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
-  before_save { |user| user.email = email.downcase }
+  #http://stackoverflow.com/questions/15520151/rails-4-authentication
+  #private
+  #
+  #def user_params
+  #  params.require(:user).permit( :password, :password_confirmation)
+  #end
 end
